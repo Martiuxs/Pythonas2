ee26df594a5731505397b512d47bb6d707cf2f28
Martiuxs <kompoprofas@gmail.com>
2023-07-14 15:12:39 +0300

    Dynamic Update

--- AT.py
+++ AT.py
@@ -5,6 +5,7 @@ from colorama import Fore, Style
 import json
 import sys
 import time
+import os
 
 def load_config(file_path):
     try:
@@ -21,20 +22,51 @@ def connect_to_device():
     except serial.SerialException as e:
         print(f"Error: Failed to connect to the device. {e}")
         return None
+    
+def move (y, x):
+    print("\033[%d;%dH" % (y, x))
+       
+def expected_return(command, expected_result, response):
+    resultFound = response.find(expected_result)
+    if resultFound != -1:
+        return f"\rTesting Command {command} | Command: Passed | Expected results: {expected_result} | Received Results: {response[resultFound:len(response)]}"
+    else:
+        return f"\rTesting Command {command} | Command: Failed | Expected results: {expected_result} | Received Results: {response.strip()}"
 
 def send_at_commands(ser, commands, total_commands):
     responses = []
+    passed_commands = 0
+    failed_commands = 0
     for i, command_data in enumerate(commands, 1):
         command = command_data['command']
         expected_result = command_data['expects']
 
-        sys.stdout.write(f"\rTesting command {command_data['command']}...  ")
-        sys.stdout.flush()
+
 
         ser.write(f'{command}\r\n'.encode('ascii'))
-        time.sleep(0.5)
+        time.sleep(0.4)
         response = ser.read_until(b'OK\r\n').decode('ascii').strip()
         responses.append((command, expected_result, response))
+        
+        # sys.stdout.write(f"\rTesting command {command_data['command']}...")
+        os.system("clear")
+        sys.stdout.write(f"\r{expected_return(command, expected_result, response)}")
+        # Testing Command AT+CGSN | Command: Passed | Expected results:OK | Received Results: OK turėtu maždaug taip atrodyt kažkas pnš
+        sys.stdout.flush()
+        
+        if response.find(expected_result) != -1:
+            passed_commands += 1
+        else:
+            failed_commands += 1
+
+        # Clear the previous test output
+
+        # Print the updated test summary
+        move(5, 0)
+        sys.stdout.write(f"{' ' * 30}Test Summary - {Fore.GREEN}Passed: {passed_commands} |{Fore.RED} Failed: {failed_commands} |{Style.RESET_ALL} Total: {passed_commands + failed_commands}")
+        sys.stdout.flush()
+        
+        
 
     sys.stdout.write("\n")
     sys.stdout.flush()
@@ -84,10 +116,14 @@ def run_at_command_tests(config_file, product_name):
     failed_commands = len(responses) - passed_commands
     total_commands = len(responses)
 
-    # Display final summary
-    print(f"\nTest Summary - {Fore.GREEN}Passed: {passed_commands} |{Fore.RED} Failed: {failed_commands} |{Style.RESET_ALL} Total: {total_commands}")
+
+    # Print the final test summary with additional spaces
+    sys.stdout.write("\n")
+    sys.stdout.flush()
 
 config_file = 'Commands.json'
 product_name = 'TRM240'
 
 run_at_command_tests(config_file, product_name)
+
+# Received Result and Expected Result and Failed or Passed 3-4 dynamic lines
